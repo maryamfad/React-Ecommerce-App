3375f6786a18c2e4cfd4dfbde18132f9261f1e2a
Maryam Fadaee <maryamfadaee@gmail.com>
2025-04-24 19:23:21 -0400

    create productItem component

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
 	const { products } = useContext(ShopContext);
@@ -9,6 +10,7 @@ const LatestCollection = () => {
 	useEffect(() => {
 		setLatestProducts(products.slice(0, 10));
 	}, []);
+
 	return (
 		<div className="my-10">
 			<div className="text-center py-8 text-3xl">
@@ -20,6 +22,18 @@ const LatestCollection = () => {
 					printer took a galley of type and scrambled it to make a
 					type specimen book.
 				</p>
+			</div>
+			{/* Renderin Products */}
+			<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+				{latestProducts?.map((item, index) => (
+					<ProductItem
+						key={index}
+						id={item._id}
+						image={item.image}
+						name={item.name}
+						price={item.price}
+					/>
+				))}
 			</div>
 		</div>
 	);
