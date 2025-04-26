db7f91c7df0aa18c28f94f23876861897892ff7d
Maryam Fadaee <maryamfadaee@gmail.com>
2025-04-26 17:49:33 -0400

    add search functionality

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 const Collection = () => {
-	const { products } = useContext(ShopContext);
+	const { products, search, showSearch } = useContext(ShopContext);
 	const [showFilter, setShowFilter] = useState(false);
 	const [filteredProducts, setFilteredProducts] = useState([]);
 	const [category, setCategory] = useState([]);
@@ -33,6 +33,12 @@ const Collection = () => {
 
 	const applyFilter = () => {
 		let productsCopy = products.slice();
+
+		if (showSearch && search) {
+			productsCopy = productsCopy.filter((item) =>
+				item.name.toLowerCase().includes(search.toLowerCase())
+			);
+		}
 		if (category.length > 0) {
 			productsCopy = productsCopy.filter((item) =>
 				category.includes(item.category)
@@ -69,7 +75,7 @@ const Collection = () => {
 
 	useEffect(() => {
 		applyFilter();
-	}, [category, subCategory]);
+	}, [category, subCategory, search, showSearch]);
 
 	useEffect(() => {
 		sortProducts();
