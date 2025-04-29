86d0eeb6d1c7ec758c0288720d22862b7a63cc27
Maryam Fadaee <maryamfadaee@gmail.com>
2025-04-29 16:35:32 -0400

    create order page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
 	const [method, setMethod] = useState("cod");
+	const { navigate } = useContext(ShopContext);
 	return (
 		<div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
 			{/* Left Side */}
@@ -119,6 +121,14 @@ const PlaceOrder = () => {
 								CASH ON DELIVERY
 							</p>
 						</div>
+					</div>
+					<div className="w-full text-end mt-8">
+						<button
+							onClick={() => navigate("orders")}
+							className="bg-black text-white px-16 py-3 text-sm"
+						>
+							PLACE ORDER
+						</button>
 					</div>
 				</div>
 			</div>
