b44dfc99caa60e421be9fa1a6e75ec8c9bae9da7
Maryam Fadaee <maryamfadaee@gmail.com>
2025-04-29 15:27:12 -0400

    add method selector functionality

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/assets";
 
 const PlaceOrder = () => {
+	const [method, setMethod] = useState("cod");
 	return (
 		<div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
 			{/* Left Side */}
@@ -72,9 +73,14 @@ const PlaceOrder = () => {
 					<Title text1={"PAYMENT"} text2={"METHOD"} />
 					{/* Payment Method Selection */}
 					<div className="flex gap-3 flex-col lg:flex-row">
-						<div className="flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer">
+						<div
+							onClick={() => setMethod("stripe")}
+							className="flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer"
+						>
 							<p
-								className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full`}
+								className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full ${
+									method === "stripe" ? "bg-green-400" : ""
+								}`}
 							>
 								{" "}
 							</p>
@@ -84,19 +90,29 @@ const PlaceOrder = () => {
 								alt="strip logo"
 							/>
 						</div>
-						<div className="flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer">
+						<div
+							onClick={() => setMethod("razorpay")}
+							className="flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer"
+						>
 							<p
-								className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full`}
+								className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full ${
+									method === "razorpay" ? "bg-green-400" : ""
+								}`}
 							></p>
 							<img
 								className="h-5 mx-4"
 								src={assets.razorpay_logo}
-								alt="strip logo"
+								alt="razorpay logo"
 							/>
 						</div>
-						<div className="flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer">
+						<div
+							onClick={() => setMethod("cod")}
+							className="flex items-center gap-3 border border-gray-300 p-2 px-3 cursor-pointer"
+						>
 							<p
-								className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full`}
+								className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full ${
+									method === "cod" ? "bg-green-400" : ""
+								}`}
 							></p>
 
 							<p className="text-gray-500 text-sm font-medium mx-4">
