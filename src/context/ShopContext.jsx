1c1f9c52b321ebf51627113fd5d14d02ba2230ac
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-05 17:08:47 -0400

    add login and sign up functionality using backend apis

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -12,6 +12,7 @@ const ShopContextProvider = (props) => {
 	const [showSearch, setShowSearch] = useState(false);
 	const [cartItems, setCartItems] = useState({});
 	const [products, setProducts] = useState([]);
+	const [token, setToken] = useState("");
 	const navigate = useNavigate();
 
 	const addToCart = async (itemId, size) => {
@@ -92,6 +93,12 @@ const ShopContextProvider = (props) => {
 	useEffect(() => {
 		getProductsData();
 	}, []);
+
+	useEffect(() => {
+		if (!token && localStorage.getItem("token")) {
+			setToken(localStorage.getItem("token"));
+		}
+	}, []);
 	const value = {
 		products,
 		currency,
@@ -107,6 +114,8 @@ const ShopContextProvider = (props) => {
 		getCartAmount,
 		navigate,
 		backendURL,
+		token,
+		setToken,
 	};
 
 	return (
