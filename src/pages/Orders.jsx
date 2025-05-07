2112d6367c58c4566c392b8d0b8ced390db7d15a
Maryam Fadaee <maryamfadaee@gmail.com>
2025-05-07 11:51:10 -0400

    get order info from backend

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Orders = () => {
-	const { products, currency } = useContext(ShopContext);
+	const { backendURL, token, currency } = useContext(ShopContext);
+	const [orderData, setOrderData] = useState([]);
+
+	const fetchOrderData = async () => {
+		try {
+			if (!token) {
+				return null;
+			} else {
+				const response = await axios.post(
+					backendURL + "/api/order/userOrders",
+					{},
+					{ headers: { token } }
+				);
+
+				if (response.data.success) {
+					let allOrdersItem = [];
+					response.data.orders.map((order) => {
+						order.items.map((item) => {
+							item["status"] = order.status;
+							item["payment"] = order.payment;
+							item["paymentMethod"] = order.paymentMethod;
+							item["date"] = order.date;
+							allOrdersItem.push(item);
+						});
+					});
+
+					setOrderData(allOrdersItem.reverse());
+				}
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
+	};
+
+	useEffect(() => {
+		fetchOrderData();
+	}, [token]);
 
 	return (
 		<div className="bordert pt-16">
@@ -12,7 +51,7 @@ const Orders = () => {
 			</div>
 
 			<div>
-				{products.slice(1, 4).map((item, index) => (
+				{orderData.map((item, index) => (
 					<div
 						key={index}
 						className="py-4 border-t text-gray-700  gap-4"
@@ -27,18 +66,24 @@ const Orders = () => {
 								<p className="sm:text-base font-medium">
 									{item.name}
 								</p>
-								<div className="flex items-center gap-3 mt-2 text-base text-gray-700">
-									<p className="text-lg">
+								<div className="flex items-center gap-3 mt-1 text-base text-gray-700">
+									<p>
 										{currency}
 										{item.price}
 									</p>
-									<p>Quantity: 1</p>
-									<p>Size: M</p>
+									<p>Quantity: {item.quantity}</p>
+									<p>Size: {item.size}</p>
 								</div>
-								<p className="mt-2">
+								<p className="mt-1">
 									Date:{" "}
 									<span className="text-gray-400">
-										25, Jul, 2025
+										{new Date(item.date).toDateString()}
+									</span>
+								</p>
+								<p className="mt-1">
+									Payment:{" "}
+									<span className="text-gray-400">
+										{item.paymentMethod}
 									</span>
 								</p>
 							</div>
@@ -46,10 +91,13 @@ const Orders = () => {
 								<div className="flex items-center gap-2">
 									<p className="min-w-2 h-2 rounded-full bg-green-500"></p>
 									<p className="text-sm md:text-base">
-										Ready to ship
+										{item.status}
 									</p>
 								</div>
-								<button className="border border-gray-300 px-4 py-2 text-sm font-medium rounded-sm">
+								<button
+									onClick={fetchOrderData}
+									className="border border-gray-300 px-4 py-2 text-sm font-medium rounded-sm"
+								>
 									Track Order
 								</button>
 							</div>
--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -81,7 +81,10 @@ const PlaceOrder = () => {
 				default:
 					break;
 			}
-		} catch (error) {}
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
 	};
 	return (
 		<form
